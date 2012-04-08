5491cb67a6e85931ba5e09674fd2e4bbc59db876
vksilva.oliveira@gmail.com <vksilva.oliveira@gmail.com@5d2b76a3-a1f9-b7dc-ff98-6b96d35d87ae>
2012-04-08 22:44:23 +0000

    tabela de simbolos riariaria espero que esteja certa manolos *-*

--- trunk/src/compiler/symbol/Symbol.java
+++ trunk/src/compiler/symbol/Symbol.java
@@ -6,16 +6,19 @@ import java.util.Hashtable;
 public class Symbol {
 	private String name;
 
+	//Dicionário: compartilhado entre todos os objetos symbol.
 	private static Dictionary<String, Symbol> dict = new Hashtable<String, Symbol>();
 
 	private Symbol(String n) {
 		name = n;
 	}
 
+	//Mapeia symbol -> string
 	public String toString() {
 		return name;
 	}
 
+	//Mapeia string -> symbol. se não existir, cria e adiciona ao dicionário.
 	public static Symbol symbol(String n) {
 		String u = n.intern();
 		Symbol s = (Symbol) dict.get(u);
--- trunk/src/compiler/symbol/Table.java
+++ trunk/src/compiler/symbol/Table.java
@@ -1,31 +1,47 @@
 package compiler.symbol;
 
 import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Stack;
 
 public class Table {
-	
-	public Table(){
-		
-		
+
+	private Hashtable<Symbol, BucketHead> hashtable;
+	private Stack<Symbol> stack;
+	private static Symbol beginScope = Symbol.symbol(null);
+
+	public Table() {
+
 	}
-	
-	public void put( Symbol key , Object value ){
+
+	public void put(Symbol key, Object value) {
+		stack.add(key);
+		hashtable.get(key).add(key, value);
+	}
+
+	public Object get(Symbol key) {
+		return hashtable.get(key).get().binding;
+	}
+
+	public void beginScope() {
+		stack.add(beginScope);
+	}
+
+	public void endScope() {
 		
+		Symbol first = stack.firstElement();
+		while(first != beginScope){
+			hashtable.get(first).pop();
+			stack.pop();
+			first = stack.firstElement();
+		}
+		if(first == beginScope){
+			stack.pop();
+		}
+
 	}
-	
-	public Object get( Symbol key ){
-		return null;
-	}
-	
-	public void beginScope(){
-		
-	}
-	
-	public void endScope(){
-		
-	}
-	
-	public Enumeration keys(){
+
+	public Enumeration keys() {
 		return null;
 	}
 }
