7cd28fdb33a8587c0bec07e34e3fdbc5c9e21b85
romulogadelhaml@gmail.com <romulogadelhaml@gmail.com@5d2b76a3-a1f9-b7dc-ff98-6b96d35d87ae>
2012-04-18 00:37:29 +0000

    Modificações nos atributos da classe Table, adição de novos métodos na classe TypeCheckVisitor.

--- trunk/src/compiler/symbol/Bucket.java
+++ trunk/src/compiler/symbol/Bucket.java
@@ -1,14 +1,15 @@
 package compiler.symbol;
 
 public class Bucket {
+	
 	Symbol key;
 	Object binding;
 	Bucket next;
 	
-	public Bucket(Symbol k, Object b, Bucket n){
-		key = k;
-		binding = b;
-		next = n;
+	public Bucket(Symbol key, Object binding, Bucket next){
+		this.key = key;
+		this.binding = binding;
+		this.next = next;
 	}
 
 }
